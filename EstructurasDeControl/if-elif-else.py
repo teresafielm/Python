facc69a74d8d43c161ecd95bc23f471ec9c63465
Teresa Fiel Muñoz <37962089+teresafielm@users.noreply.github.com>
2023-01-23 13:04:51 -0600

    Create if-elif-else.py

---
+++ EstructurasDeControl/if-elif-else.py
@@ -0,0 +1,8 @@
+num = float(input("Ingresa un número: ")) 
+if num > 0: 
+    result = "El número es positivo" 
+elif num < 0: 
+    result = "El número es negativo" 
+else: 
+    result = "El número es cero" 
+print(result)
