9a0171e92d22001fb1309afdc6717ac7d539506d
Teresa Fiel Muñoz <37962089+teresafielm@users.noreply.github.com>
2023-01-23 13:10:21 -0600

    Create mlx.py

---
+++ Biblioteca/mlx.py
@@ -0,0 +1,7 @@
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+print ("Temperatura ambiente :", sensor.get_amb_temp())
+print ("Temperatura de Persona u objeto :", sensor.get_obj_temp())
+bus.close()
